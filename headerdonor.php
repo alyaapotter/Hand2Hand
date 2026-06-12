b5e76da60d7fae667a162b88d9897f8376dc4e0f
alyaapotter <alyaapotter@gmail.com>
2026-06-12 14:02:07 +0800

    update: header (donor) php

---
+++ headerdonor.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header Donor</title>
+    <link rel="stylesheet" type="text/css" href="format.css">
+</head>
+
+<body>
+
+    <header class="header2">
+
+        <div class="top-header">
+            <div class="logo">
+                <img src="logo.png" alt="Hand2Hand logo" width="80">
+                <h1>Hand2Hand</h1>
+            </div>
+            <a href="logout.php">
+                <button class="logout-btn">Logout</button>
+            </a>
+        </div>
+
+        <nav>
+            <a href="#">Home</a> |
+            <a href="eventdonor.html">My Events</a> |
+            <a href="#">My Donations</a>
+        </nav>
+
+    </header>
+</body>
+
+</html>
