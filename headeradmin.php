d23baeb7f944c1aee7e11be8e635ba9901eff82d
alyaapotter <alyaapotter@gmail.com>
2026-06-12 14:01:19 +0800

    update: header (admin) php

---
+++ headeradmin.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header Admin</title>
+    <link rel="stylesheet" type="text/css" href="format.css">
+</head>
+
+<body>
+
+    <header class="header2">
+
+        <div class="top-header">
+            <div class="logo">
+                <img src="logo.png" alt="Hand2Hand logo" width="80">
+                <h1>Hand2Hand</h1>
+            </div>
+            <a href="logout.php">
+                <button class="logout-btn">Logout</button>
+            </a>
+        </div>
+
+        <nav>
+            <a href="#">Dashboard</a> |
+            <a href="#">Beneficiaries</a> |
+            <a href="donation_event_admin.html">Events</a> |
+            <a href="#">Inventory</a> |
+            <a href="#">Distribution</a>
+        </nav>
+
+    </header>
+</body>
+
+</html>
