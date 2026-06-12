4d3603be26206106233d734122df89bd083119f3
alyaapotter <alyaapotter@gmail.com>
2026-06-12 14:02:31 +0800

    update: header (main) php

---
+++ headermain.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Header Main</title>
+    <link rel="stylesheet" type="text/css" href="format.css">
+</head>
+
+<body>
+
+    <header class="header1">
+
+        <div class="logo">
+            <img src="logo.png" alt="Hand2Hand logo" width="80">
+        </div>
+
+        <nav>
+            <a href="home.html">Hand2Hand</a> |
+            <a href="about.html">About Us</a> |
+            <a href="event.html">Events</a> |
+            <a href="login.html">Login</a>
+        </nav>
+
+    </header>
+</body>
+
+</html>
