ebb4616dafd43d90487aa6b15de5829f6b7d0759
mirrahfarhana <mirrahismanizal@gmail.com>
2026-06-30 14:12:33 +0800

    fix table css

--- includes/connect.php
+++ includes/connect.php
@@ -1,18 +1,16 @@
 <?php
-// includes/db.php
-// Database connection for Hand2Hand system
-
 $servername = "localhost";
 $username = "root";
-$password = "";
+$password = "root123";
 $dbname = "hand2hand";
-$port = 3307;
 
-$conn = new mysqli($servername, $username, $password, $dbname, $port);
-
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname, 3307 );
+ 
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-}
+} 
+//echo "Connected successfully";
 
-// Set character encoding
-$conn->set_charset("utf8");
+?>
