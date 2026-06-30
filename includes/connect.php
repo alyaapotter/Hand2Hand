407b2aac9655f29dcb77a5d0274e2834867eab84
Damilia <d4milia@gmail.com>
2026-06-30 13:03:57 +0800

    fixed connect.php port

--- includes/connect.php
+++ includes/connect.php
@@ -6,7 +6,7 @@ $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "hand2hand";
-$port = 3301;
+$port = 3307;
 
 $conn = new mysqli($servername, $username, $password, $dbname, $port);
 
